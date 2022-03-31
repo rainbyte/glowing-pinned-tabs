0c180de26273ac2bf22de92871937a73673250c4
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2022-03-31 14:26:00 -0300

    Support pages which rely on favicon.ico

--- content-script.js
+++ content-script.js
@@ -27,12 +27,24 @@ chrome.runtime.sendMessage({ request: "isPinnedTab?" }, isPinned => {
 
         // Fill space anyways when there is no icon
         if (!nodes || nodes.length == 0) {
-            ctx.fillStyle = "#FF00FF";
-            ctx.fillRect(0, 0, canvas.width, 8);
             link = document.createElement('link');
             link.rel = 'icon';
-            link.href = canvas.toDataURL();
-            document.getElementsByTagName('head')[0].appendChild(link);
+
+            var favicon = new Image(32, 32);
+            favicon.src = "/favicon.ico";
+            favicon.onload = function () {
+                ctx.drawImage(favicon, 0, 0, canvas.width, canvas.height);
+                ctx.fillStyle = "#FF00FF";
+                ctx.fillRect(0, 0, canvas.width, 8);
+                link.href = canvas.toDataURL();
+                document.getElementsByTagName('head')[0].appendChild(link);
+            };
+            favicon.onerror = function () {
+                ctx.fillStyle = "#FF00FF";
+                ctx.fillRect(0, 0, canvas.width, 8);
+                link.href = canvas.toDataURL();
+                document.getElementsByTagName('head')[0].appendChild(link);
+            };
         }
     }
 });
